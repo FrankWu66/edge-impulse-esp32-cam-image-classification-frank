483d644f1133a8c9282059d52142a4279cf07ea9
frankwu <frank_wu@quantatw.com>
2024-05-16 17:18:40 +0800

    python for image

--- python/Cloud_handler.py
+++ python/Cloud_handler.py
@@ -8,13 +8,45 @@ MqttPort=1883
 CloudTopic="frank/Clould_to_Edge"
 EdgeTopic="frank/Edge_to_Cloud"
 ISOTIMEFORMAT = '%H:%M:%S.%f'
- 
+
+index = 0
+time_start = 0
+time_end = 0
+
 #Callback for connect.
 def on_connect(client, userdata, flags, reason_code, properties):
     print("Cloud 連線結果：" + str(reason_code) + "\n")
     #訂閱Edge傳來的TOPIC
     client.subscribe(EdgeTopic)
-    
+
+#callback for receive subscribe update.
+def on_message(client, userdata, message):
+    global index
+    global time_start
+    global time_end
+    index+=1
+    filename = 's_%d.jpg' % index
+    time_start = time.time()
+    print('received from Edge: %s' % (datetime.datetime.now().strftime(ISOTIMEFORMAT)))
+    f = open(filename,'wb+')
+    f.write(message.payload)
+    f.close()
+
+    img=cv2.imread(filename)
+    img=cv2.resize(img, (640, 480))
+    cv2.imshow('show in clould', img)
+    key=cv2.waitKey(1)  # wait 1ms for keyboard...
+    #cv2.destroyWindow('image')
+
+    payload = 'C2E_' + str(index) + "_" + datetime.datetime.now().strftime(ISOTIMEFORMAT)
+    client.publish(CloudTopic, payload)
+    time_end = time.time()
+    time_interval = time_end - time_start
+    print ("finish send message:" + payload)
+    print ('spend time (in cloud): %.6f' % time_interval)
+    print (" ")
+
+'''
 #callback for receive subscribe update.
 def on_message(client, userdata, message):
     print('received from Edge: [%s]: %s' % (message.payload, datetime.datetime.now().strftime(ISOTIMEFORMAT)))
@@ -22,7 +54,7 @@ def on_message(client, userdata, message):
     client.publish(CloudTopic, payload)
     print ("finish send" + datetime.datetime.now().strftime(ISOTIMEFORMAT))
     print ("\n")
-
+'''
  
 #setting MQTT connect   
 mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
--- python/Edge.py
+++ python/Edge.py
@@ -1,5 +1,5 @@
 import paho.mqtt.client as mqtt
-import random
+import cv2
 import datetime 
 import time
 
@@ -9,15 +9,24 @@ CloudTopic="frank/Clould_to_Edge"
 EdgeTopic="frank/Edge_to_Cloud"
 ISOTIMEFORMAT = '%H:%M:%S.%f'
 
+index = 0
+time_start = 0
+time_end = 0
+
 #Callback for connect.
 def on_connect(client, userdata, flags, reason_code, properties):
     print("Edge 連線結果：" + str(reason_code) + "\n")
     #訂閱Cloud傳來的TOPIC
     client.subscribe(CloudTopic)
-    
+
 #callback for receive subscribe update.
 def on_message(client, userdata, message):
-    print('received from Cloud: [%s]: %s' % (message.payload, datetime.datetime.now().strftime(ISOTIMEFORMAT)))
+    global time_start
+    global time_end
+    print ('received from Cloud: [%s]: %s' % (message.payload, datetime.datetime.now().strftime(ISOTIMEFORMAT)))
+    time_end = time.time()
+    time_interval = time_end - time_start
+    print ('spend time (total): %.6f' % time_interval)
     print (" ")
 
 mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
@@ -27,8 +36,7 @@ mqttc.connect(MqttBroker, MqttPort, 60)
 mqttc.loop_start()
 time.sleep(1)
 
-index = 1
-
+'''
 while True:
     payload = "===" + str(index) + "=="
     index+=1
@@ -36,4 +44,17 @@ while True:
     mqttc.publish(EdgeTopic, payload)
     print ("finish send" + datetime.datetime.now().strftime(ISOTIMEFORMAT))
     #mqttc.loop()
+    time.sleep(3)
+'''
+
+while True:
+    index+=1
+    if index > 6:
+        index = 1
+    img=cv2.imread(str(index) + '.jpg')
+    byteArr = cv2.imencode('.jpg', img)[1].tobytes()
+    print ("send " + str(index) + ".jpg start:" + datetime.datetime.now().strftime(ISOTIMEFORMAT))
+    time_start = time.time()
+    mqttc.publish(EdgeTopic, byteArr)
+    #print ("finish send" + datetime.datetime.now().strftime(ISOTIMEFORMAT))
     time.sleep(3)
