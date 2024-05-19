7c85911cf2dc61aee43175e3f14a225b28b303d8
FrankWu66 <iauz.gg@gmail.com>
2024-05-19 22:33:17 +0800

    add result pics and modified python

--- python/Cloud_handler.py
+++ python/Cloud_handler.py
@@ -3,12 +3,16 @@ import cv2
 import datetime 
 import time
 
-#MqttBroker="127.0.0.1"
+MqttBroker="127.0.0.1"
 #MqttBroker="mqttgo.io"
-MqttBroker="mqtt.eclipseprojects.io"
+#MqttBroker="mqtt.eclipseprojects.io"
 MqttPort=1883
 CloudTopic="frank/Clould_to_Edge"
 EdgeTopic="frank/Edge_to_Cloud"
+
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+ubody_cascade = cv2.CascadeClassifier("haarcascade_upperbody.xml")
+
 ISOTIMEFORMAT = '%H:%M:%S.%f'
 
 index = 0
@@ -26,21 +30,45 @@ def on_message(client, userdata, message):
     global index
     global time_start
     global time_end
+    detectFace = False
+    detectUbody = False
+
     index+=1
-    filename = 's_%d.jpg' % index
+    #filename = 's_%d.jpg' % index
+    filename = 's_%d.bmp' % index
     time_start = time.time()
     print('received from Edge: %s' % (datetime.datetime.now().strftime(ISOTIMEFORMAT)))
     f = open(filename,'wb+')
     f.write(message.payload)
     f.close()
 
-    img=cv2.imread(filename)
-    img=cv2.resize(img, (640, 480))
+    img = cv2.imread(filename)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # transfer to gray img
+
+    # detect face
+    faces = face_cascade.detectMultiScale(gray)
+    if len(faces):
+        detectFace = True
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)   # mark face by green color #BGR
+        cv2.putText(img, 'face', (x,y-5),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0), 1, cv2.LINE_AA)
+
+    # detect upper body
+    ubodys = ubody_cascade.detectMultiScale(gray)
+    if len(ubodys):
+        detectUbody = True
+    for (x, y, w, h) in ubodys:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)   # mark ubody by blue color
+        cv2.putText(img, 'upper body', (x,y-5),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,0,0), 1, cv2.LINE_AA)
+
+    #img=cv2.resize(img, (640, 480))
+    img=cv2.resize(img, (576, 576))
     cv2.imshow('show in clould', img)
     key=cv2.waitKey(1)  # wait 1ms for keyboard...
     #cv2.destroyWindow('image')
 
-    payload = 'C2E_' + str(index) + "_" + datetime.datetime.now().strftime(ISOTIMEFORMAT)
+    #payload = 'C2E_' + str(index) + "_" + datetime.datetime.now().strftime(ISOTIMEFORMAT)
+    payload = 'C2E_' + str(index) + ", detectFace: %d, detectUbody: %d" % (detectFace, detectUbody)
     client.publish(CloudTopic, payload)
     time_end = time.time()
     time_interval = time_end - time_start
--- python/Edge.py
+++ python/Edge.py
@@ -3,9 +3,9 @@ import cv2
 import datetime 
 import time
 
-#MqttBroker="127.0.0.1"
+MqttBroker="127.0.0.1"
 #MqttBroker="mqttgo.io"
-MqttBroker="mqtt.eclipseprojects.io"
+#MqttBroker="mqtt.eclipseprojects.io"
 MqttPort=1883
 CloudTopic="frank/Clould_to_Edge"
 EdgeTopic="frank/Edge_to_Cloud"
@@ -31,7 +31,6 @@ def on_message(client, userdata, message):
     global cycle
     global total_time
     cycle += 1
-    print (" ")
     print ('received from Cloud: [%s]: %s' % (message.payload, datetime.datetime.now().strftime(ISOTIMEFORMAT)))
     time_end = time.time()
     time_interval = time_end - time_start[cycle-1]
@@ -58,21 +57,26 @@ while True:
     time.sleep(1)
 '''
 
-while publish_count < 300:
+while publish_count < 100:
+    #hile True:
     index+=1
     publish_count += 1
-    if index > 1:
+    if index > 6:
         index = 1
-    img=cv2.imread(str(index) + '.jpg')
-    byteArr = cv2.imencode('.jpg', img)[1].tobytes()
-    #print ("send " + str(index) + ".jpg start:" + datetime.datetime.now().strftime(ISOTIMEFORMAT))
+    #img=cv2.imread(str(index) + '.jpg')
+    img=cv2.imread('_1.bmp')
+    #byteArr = cv2.imencode('.jpg', img)[1].tobytes()
+    byteArr = cv2.imencode('.bmp', img)[1].tobytes()
+    print ("send " + str(index) + ".jpg start:" + datetime.datetime.now().strftime(ISOTIMEFORMAT))
     #time_start= time.time()
     time_start.append(time.time())
     mqttc.publish(EdgeTopic, byteArr)
     #print ("finish send" + datetime.datetime.now().strftime(ISOTIMEFORMAT))
-    #time.sleep(2)
+    time.sleep(1)
 
+'''
 while cycle < 300:
     time.sleep(1)
 
 time.sleep(3)
+'''
